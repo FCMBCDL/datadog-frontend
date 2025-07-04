ebc9504527bbd82138541289d1350bc9c3652dd4
Sarah <mokwesarah@fcmb.com>
2025-07-04 11:41:58 +0100

    update datadog implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
+import "./rum";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { reactPlugin } from '@datadog/browser-rum-react'
-import { datadogRum } from '@datadog/browser-rum'
+
 
 function App() {
   const [count, setCount] = useState(0)
-
-
-  datadogRum.init({
-    applicationId: '3de1186f-a7bb-4e48-b171-6d1e92c94b64',
-    clientToken: 'pubeb9dfb7b2f593dfbc00c38ae43f41140',
-    site: 'datadoghq.eu',
-    service:'usscp-test-1',
-    env: 'test',
-    // Specify a version number to identify the deployed version of your application in Datadog
-    // version: '1.0.0',
-    sessionSampleRate:  100,
-    sessionReplaySampleRate: 100,
-    defaultPrivacyLevel: 'mask-user-input',
-    plugins: [reactPlugin({ router: true })],
-});
 
   return (
     <>
